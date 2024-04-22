3e80e6d3c46f7a95d7c35dc9a2311d7a94f6448f
ALPHA2509 <125252209+ALPHA2509@users.noreply.github.com>
2024-04-22 15:43:27 +0530

    Group No 10

---
+++ Group 10 Mini Project HealthScan AI/Health AI/lib/common/widgets/error.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({
+    Key? key,
+    required this.error,
+  }) : super(key: key);
+  final String error;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(error),
+      ),
+    );
+    ;
+  }
+}
